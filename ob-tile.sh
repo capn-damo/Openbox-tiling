e14d96562d4f347e9117b80ecb25d56d50a721b9
capn-damo <damo@bunsenlabs.org>
2017-08-04 23:27:22 +0100

    Add functions for saving window positions, and restoring them

--- ob-tile.sh
+++ ob-tile.sh
@@ -4,7 +4,6 @@
 #
 #   Requires wmctrl and xdotool
 
-TMPFILE=$(mktemp --tmpdir winlist.XXXX)
 USAGE='
     ob-tile.sh [arg]
     
@@ -19,6 +18,10 @@ USAGE='
         
     With no script arguments the windows will be grid tiled.
 '
+
+TMP_WIN_LIST=$(mktemp --tmpdir winlist.XXXX)
+TMP_WIN_DIMS="$HOME/temp-windims.txt"
+
 arrGRIDKBINDS=(super+alt+7 super+alt+9 super+alt+3 super+alt+1 super+alt+5)
 arrVERTKBINDS=(super+alt+4 super+alt+6)
 arrHORIZKBINDS=(super+alt+8 super+alt+2)
@@ -54,7 +57,7 @@ tileWindows(){
 tileVert(){
     local arg="V"
     arrL=${#arrWIN_ID[@]}
-    if (( $arrL < 3 ));then
+    if (( $arrL < 3 )) &>/dev/null;then
         tileWindows $arrL $arg
     else
         echo "Only 2 windows should be on the desktop"
@@ -67,7 +70,7 @@ ob-tile.sh requires only 2 windows."
 tileHoriz(){
     local arg="H"
     arrL=${#arrWIN_ID[@]}
-    if (( $arrL < 3 ));then
+    if (( $arrL < 3 )) &>/dev/null;then
         tileWindows $arrL $arg
     else
         echo "Only 2 windows should be on the desktop"
@@ -83,8 +86,55 @@ tileGrid(){
     tileWindows $arrL $arg  
 }
 
+getWM_VALUES(){         # get frame and window geometry set by WM
+    
+    declare -a arrFRAME_EXTENTS arrWIN_EXTENTS
+    
+    arrFRAME_EXTENTS=( $(xprop -id $id _NET_FRAME_EXTENTS | awk ' {gsub(/,/,"");print $3,$4,$5,$6}') )
+    BORDER_L="${arrFRAME_EXTENTS[0]}"
+    BORDER_R="${arrFRAME_EXTENTS[1]}"
+    BORDER_T="${arrFRAME_EXTENTS[2]}"
+    BORDER_B="${arrFRAME_EXTENTS[3]}"
+    
+    arrWIN_EXTENTS=( $(xwininfo -id $id | grep -E "(Absolute|Width|Height)" | awk '{print $NF}') )
+    UPPER_L_X="${arrWIN_EXTENTS[0]}"
+    UPPER_L_Y="${arrWIN_EXTENTS[1]}"
+    WIDTH="${arrWIN_EXTENTS[2]}"
+    HEIGHT="${arrWIN_EXTENTS[3]}"
+    
+    posX=$((UPPER_L_X - BORDER_L))
+    posY=$((UPPER_L_Y - BORDER_T))
+}
+
+# save window id's and geometry, for restoring with wmctrl.
+getWindows(){   
+    for id in "${arrWIN_ID[@]}";do
+        getWM_VALUES
+        wmctrlCMD="$id -e 0,$posX,$posY,$WIDTH,$HEIGHT"
+        echo "$wmctrlCMD" >> "$TMP_WIN_DIMS"
+    done
+}
+
+restoreWindows(){
+    while read line;do
+        wmctrl -ir $line
+    done < "$TMP_WIN_DIMS"
+}
+
+# test if tempfile exists, containing geometry etc needed by wmctrl.
+testRestore(){
+    if [[ ! -f "$TMP_WIN_DIMS" ]] &>/dev/null ;then
+        touch "$TMP_WIN_DIMS"
+        getWindows
+    else
+        restoreWindows
+        rm "$TMP_WIN_DIMS"
+        exit 0
+    fi    
+}
+
 # get window list, save to tempfile
-wmctrl -lx > "$TMPFILE"
+wmctrl -lx > "$TMP_WIN_LIST"
 
 declare -a arrWIN_ID
 
@@ -94,18 +144,22 @@ CURRDTOP=$(xprop -root _NET_CURRENT_DESKTOP | tail -c -2) # desktop number
 # loop through windows, send OB keybinds to each
 i=0
 while read line;do
-    if grep -Eq -v "(Conky|Tint2)" "$TMPFILE";then
-        if [[ $(echo $line | awk '{print $2}') == $CURRDTOP ]];then
+    if grep -Eq -v "(Conky|Tint2)" "$TMP_WIN_LIST" ;then
+        if [[ $(echo $line | awk '{print $2}') == $CURRDTOP ]] &>/dev/null;then
             arrWIN_ID[$i]=$(echo $line | awk '{print $1}')
             ((i+=1))
         fi
     fi
-done < "$TMPFILE"
+done < "$TMP_WIN_LIST"
+
+# see if we are toggling the window positions to their original placement.
+testRestore
 
 if (( "$#" == 0 ));then
-    tileGrid            # no script args
+    tileGrid            # no script args, so just do a grid tile.
 else
     findArgs "$@"
 fi
 
-rm "$TMPFILE"
+rm "$TMP_WIN_LIST"
+exit 0
